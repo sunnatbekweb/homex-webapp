2fa9f62fdeb4cd6ca52ed867c70168df03cd1495
sunnatbekweb <sunnatbekkadamov@gmail.com>
2026-05-30 19:54:36 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Thumbnail } from './components/ui/Thumbnail'
+
+function App() {
+	return (
+		<div>
+			<Thumbnail />
+		</div>
+	)
+}
+
+export default App
---
+++ src/components/ui/Thumbnail.tsx
@@ -0,0 +1,15 @@
+export const Thumbnail = () => {
+	return (
+		<section className="auth_container">
+			<svg
+				className="icon"
+				role="presentation"
+				aria-hidden="true"
+				width="532"
+				height="118"
+			>
+				<use href="/icons/icons.svg#logo" />
+			</svg>
+		</section>
+	)
+}
